3b80dbb8cfac498313051e64c3ae1f7c69b3fa15
AsCE-hyunseung <leehs1222@gmail.com>
2019-02-27 13:47:19 +0900

    3단계 완료

---
+++ src/test/java/com/asce/webservice/service/PostServiceTest.java
@@ -0,0 +1,48 @@
+package com.asce.webservice.service;
+
+import com.asce.webservice.domain.posts.Posts;
+import com.asce.webservice.domain.posts.PostsRepository;
+import com.asce.webservice.dto.PostsSaveRequestDto;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostServiceTest {
+
+    @Autowired
+    private PostsService postsService;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void cleanup () {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Dto데이터가_posts테이블에_저장된다 () {
+        //given
+        PostsSaveRequestDto dto = PostsSaveRequestDto.builder()
+                .author("leehs1222@gmail.com")
+                .content("테스트")
+                .title("테스트 타이틀")
+                .build();
+
+        //when
+        postsService.save(dto);
+
+        //then
+        Posts posts = postsRepository.findAll().get(0);
+        assertThat(posts.getAuthor()).isEqualTo(dto.getAuthor());
+        assertThat(posts.getContent()).isEqualTo(dto.getContent());
+        assertThat(posts.getTitle()).isEqualTo(dto.getTitle());
+    }
+}
